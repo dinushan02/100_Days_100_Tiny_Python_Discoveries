34aebcb942ee1f5202c3214a23e068310d7b3f55
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-09-26 15:50:30 +0530

    Add Day 02 - Random Number Generator

---
+++ Day_02_Random_Number_Generator/random_number_generator.py
@@ -0,0 +1,10 @@
+"""
+Generate a random integer between 1 and 100
+using Python's built-in random module.
+"""
+
+import random
+
+number = random.randint(1, 100)
+
+print(f"Your random number is: {number}")
